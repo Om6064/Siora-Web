c42bce56995f973470f27d5d5641a8e1ce70b829
om6064 <aryanpatel.lihodiya@gmail.com>
2025-04-24 15:42:26 +0530

    added about us and contect

---
+++ js/cart.js
@@ -0,0 +1,42 @@
+let productArray = [
+    {
+        id:1,
+        productImage:"./images/SIORA_BRIGHTENING_BODY_LOTION-removebg-preview.png",
+        productName:"Siora Brightening Body Lotion",
+        productPrice: 749,
+        productSize:250,
+        productDescription:"A luxurious, lightweight body lotion formulated to deeply nourish, hydrate, and visibly brighten the skin. Enriched with powerhouse actives like Niacinamide and Glutathione, this lotion promotes an even skin tone while restoring your skin's natural glow",
+    },
+    {
+        id:2,
+        productImage: "./images/SIORA_CRYSTAL_CLEAR_GEL_CLEANSER-removebg-preview.png",
+        productName: "Siora Crystal Clear Gel Cleanser",
+        productPrice: 699,
+        productSize: 100,
+        productDescription: "A gentle yet powerful gel-based facial cleanser designed to purify, refresh, and restore the skin without stripping its natural moisture. Enriched with natural botanicals and soothing ingredients, it deeply cleanses pores, removes impurities, excess oil, and makeup residue — leaving the skin feeling soft, smooth, and radiantly clean. Ideal for daily use and suitable for all skin types, including sensitive skin",
+    },
+    {
+        id:3,
+        productImage:"./images/SIORA_EXFOLIATING_COFFEE_BODY_SCRUB-removebg-preview.png",
+        productName:"Siora Exfoliating Coffee Body Scrub",
+        productPrice: 699,
+        productSize:200,
+        productDescription:"Siora Exfoliating Coffee Body Scrub is a luxurious, invigorating blend crafted to gently slough away dead skin cells, promote circulation, and reveal irresistibly smooth, glowing skin. Infused with the richness of Arabica Coffee, nourishing oils, and antioxidant boosters, this scrub energizes your senses while deeply pampering your body",
+    },
+    {
+        id:4,
+        productImage:"./images/SIORA_HAIR_GROWTH_SERUM-removebg-preview.png",
+        productName:"Siora Hair Growth Serum",
+        productPrice: 899,
+        productSize: 30,
+        productDescription: "A powerful, non-greasy serum formulated to reduce hair fall and stimulate new hair growth. Enriched with clinically proven actives like Redensyl and Procapil, it works deep into the scalp to nourish follicles, strengthen roots, and promote visibly thicker, fuller hair. Ideal for both men and women experiencing thinning hair or hair loss.",
+    }
+
+]
+
+let addProduct = document.querySelectorAll(".addProduct")
+
+console.log(addProduct);
+
+// let cartArr = JSON.parse(localStorage.getItem("cart")) || [];
+
