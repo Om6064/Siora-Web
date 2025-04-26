2c12262b72d9c84f3dd2fdeb5d3c86ee805d300e
om6064 <aryanpatel.lihodiya@gmail.com>
2025-04-26 18:51:45 +0530

    done with filter feature

--- js/cart.js
+++ js/cart.js
@@ -45,7 +45,7 @@ let allProduct = [
     },
     {
         id: 2,
-        productImage: "./images/SIORA_CRYSTAL_CLEAR_GEL_CLEANSER-removebg-preview.png",
+        productImage: "./images/SIORA CRYSTAL CLEAR GEL CLEANSER.png",
         productName: "Siora Crystal Clear Gel Cleanser",
         productPrice: 699,
         productSize: 100,
@@ -53,7 +53,7 @@ let allProduct = [
     },
     {
         id: 3,
-        productImage: "./images/SIORA_EXFOLIATING_COFFEE_BODY_SCRUB-removebg-preview.png",
+        productImage: "./images/SIORA EXFOLIATING COFFEE BODY SCRUB.png",
         productName: "Siora Exfoliating Coffee Body Scrub",
         productPrice: 699,
         productSize: 200,
@@ -61,7 +61,7 @@ let allProduct = [
     },
     {
         id: 4,
-        productImage: "./images/SIORA_HAIR_GROWTH_SERUM-removebg-preview.png",
+        productImage: "./images/SIORA HAIR GROWTH SERUM.png",
         productName: "Siora Hair Growth Serum",
         productPrice: 899,
         productSize: 30,
@@ -69,7 +69,7 @@ let allProduct = [
     },
     {
         id: 5,
-        productImage: "./images/SIORA_INTENSE_REPAIR_HAIR_ELIXIR-removebg-preview.png",
+        productImage: "./images/SIORA INTENSE REPAIR HAIR ELIXIR.png",
         productName: "Siora Intense Repair Hair Elixir",
         productPrice: 799,
         productSize: 30,
@@ -149,8 +149,168 @@ let allProduct = [
     },
 ]
 
+let bodyProducts = [
+    {
+        id: 1,
+        productImage: "./images/SIORA BRIGHTENING BODY LOTION.png",
+        productName: "Siora Brightening Body Lotion",
+        productPrice: 799,
+        productSize: 250,
+        navigationLink: "./bodylotion.html"
+    },
+    {
+        id: 2,
+        productImage: "./images/SIORA EXFOLIATING COFFEE BODY SCRUB.png",
+        productName: "Siora Exfoliating Coffee Body Scrub",
+        productPrice: 699,
+        productSize: 200,
+        navigationLink: "./bodyscrum.html"
+    },
+    {
+        id: 3,
+        productImage: "./images/SIORA REFRESH & REPAIR BODY CLEANSER.png",
+        productName: "Siora Refresh & Repair Body Cleanser",
+        productPrice: 699,
+        productSize: 250,
+        navigationLink: "./bodyclenser.html"
+    },
+    {
+        id: 4,
+        productImage: "./images/SIORA SILK GLOW BODY OIL.png",
+        productName: "Siora Silk Glow Body Oil",
+        productPrice: 899,
+        productSize: 100,
+        navigationLink: "./bodyoil.html"
+    },
+]
+let scrumProducts = [
+    {
+        id: 1,
+        productImage: "./images/SIORA EXFOLIATING COFFEE BODY SCRUB.png",
+        productName: "Siora Exfoliating Coffee Body Scrub",
+        productPrice: 699,
+        productSize: 200,
+        navigationLink: "./bodyscrum.html"
+    },
+    {
+        id: 2,
+        productImage: "./images/SIORA HAIR GROWTH SERUM.png",
+        productName: "Siora Hair Growth Serum",
+        productPrice: 899,
+        productSize: 30,
+        navigationLink: "./hairgrowthserum.html"
+    },
+    {
+        id: 3,
+        productImage: "./images/SIORA RADIANCE RENEWAL SERUM.png",
+        productName: "Siora Radiance Renewal Serum",
+        productPrice: 999,
+        productSize: 30,
+        navigationLink: "./renewalserum.html"
+    },
+]
+let faceProducts = [
+    {
+        id: 1,
+        productImage: "./images/SIORA MATTE DEFENSE SUNSCREEN SPF 50 PA+++.png",
+        productName: "Siora Matte Defence Sunscreen",
+        productPrice: 799,
+        productSize: 50,
+        navigationLink: "./sunscreen.html"
+    },
+    {
+        id: 2,
+        productImage: "./images/SIORA RADIANCE RENEWAL SERUM.png",
+        productName: "Siora Radiance Renewal Serum",
+        productPrice: 999,
+        productSize: 30,
+        navigationLink: "./renewalserum.html"
+    },
+    {
+        id: 3,
+        productImage: "./images/renamed.png",
+        productName: "Siora Salicylic Acid Face Wash",
+        productPrice: 100,
+        productSize: 30,
+        navigationLink: "./facewash.html"
+    },
+    {
+        id: 4,
+        productImage: "./images/SIORA VELVET GLOW MOISTURIZER.png",
+        productName: "Siora Velvet Glow Moisturizer",
+        productPrice: 1299,
+        productSize: 50,
+        navigationLink: "./moisturizer.html"
+    },
+]
+let hairProducts = [
+    {
+        id: 1,
+        productImage: "./images/SIORA HAIR GROWTH SERUM.png",
+        productName: "Siora Hair Growth Serum",
+        productPrice: 899,
+        productSize: 30,
+        navigationLink: "./hairgrowthserum.html"
+    },
+    {
+        id: 2,
+        productImage: "./images/SIORA INTENSE REPAIR HAIR ELIXIR.png",
+        productName: "Siora Intense Repair Hair Elixir",
+        productPrice: 799,
+        productSize: 30,
+        navigationLink: "./Hairelixir.html"
+    },
+    {
+        id: 3,
+        productImage: "./images/SIORA VELVET TOUCH HAIR MASQUE.png",
+        productName: "Siora Velvet Touch Hair Masque",
+        productPrice: 799,
+        productSize: 200,
+        navigationLink: "./hairmasque.html"
+    },
+    {
+        id: 4,
+        productImage: "./images/SIORA ONION HAIR OIL.png",
+        productName: "Siora Onion Hair Oil",
+        productPrice: 699,
+        productSize: 100,
+        navigationLink: "./onionoil.html"
+    },
+    {
+        id: 5,
+        productImage: "./images/SIORA SUBLIME SHINE SHAMPOO.png",
+        productName: "Siora Sublime Shine Shampoo",
+        productPrice: 899,
+        productSize: 250,
+        navigationLink: "./sublimeshampoo.html"
+    },
+]
+let cleanserProducts = [
+    {
+        id: 1,
+        productImage: "./images/SIORA CRYSTAL CLEAR GEL CLEANSER.png",
+        productName: "Siora Crystal Clear Gel Cleanser",
+        productPrice: 699,
+        productSize: 100,
+        navigationLink: "./bodyclenser.html"
+    },
+    {
+        id: 2,
+        productImage: "./images/SIORA REFRESH & REPAIR BODY CLEANSER.png",
+        productName: "Siora Refresh & Repair Body Cleanser",
+        productPrice: 699,
+        productSize: 250,
+        navigationLink: "./bodyclenser.html"
+    },
+]
+
 let addProduct = document.getElementById("cart-content");
 let addallProduct = document.getElementById("all-content");
+let bodycontent = document.getElementById("body-content");
+let scrumcontent = document.getElementById("scrum-content")
+let facecontent = document.getElementById("face-content")
+let haircontent = document.getElementById("hair-content")
+let cleansercontent = document.getElementById("cleanser-content")
 
 productArray.forEach(item => {
     addProduct.innerHTML += `
@@ -175,23 +335,136 @@ productArray.forEach(item => {
     `;
 });
 
-allProduct.forEach((item) =>{
+allProduct.forEach((item) => {
     addallProduct.innerHTML += `
         <div class="col-md-3">
-									<div class="product-item">
-										<figure class="product-style">
-											<img src="${item.productImage}" alt="${item.productName}" class="product-item">
-											<button type="button" class="add-to-cart"
-												data-product-tile="add-to-cart">Add to
-												Cart</button>
-										</figure>
-										<figcaption>
-											<h3>${item.productName}</h3>
-											<span>${item.productSize}</span>
-											<div class="item-price">${item.productPrice}</div>
-										</figcaption>
-									</div>
-								</div>
+            <div class="product-item">
+                <figure class="product-style">
+                    <img src="${item.productImage}" alt="${item.productName}" class="product-item">
+                    <button type="button" class="add-to-cart" data-product-tile="add-to-cart" onclick="addToCart(${item.id})">Add to Cart</button>
+                </figure>
+                <a href="${item.navigationLink}">
+                    <div class="text-center m-0 p-0">
+                        <button class="border rounded">Show details</button>
+                    </div>
+                </a>
+                <figcaption>
+                    <h3>${item.productName}</h3>
+                    <span>Size: ${item.productSize}ml | 3.4 oz.</span>
+                    <div class="item-price">$${item.productPrice} (introductory)</div>
+                </figcaption>
+            </div>
+        </div>
+    `
+})
+bodyProducts.forEach((item) => {
+    bodycontent.innerHTML += `
+        <div class="col-md-3">
+            <div class="product-item">
+                <figure class="product-style">
+                    <img src="${item.productImage}" alt="${item.productName}" class="product-item">
+                    <button type="button" class="add-to-cart" data-product-tile="add-to-cart" onclick="addToCart(${item.id})">Add to Cart</button>
+                </figure>
+                <a href="${item.navigationLink}">
+                    <div class="text-center m-0 p-0">
+                        <button class="border rounded">Show details</button>
+                    </div>
+                </a>
+                <figcaption>
+                    <h3>${item.productName}</h3>
+                    <span>Size: ${item.productSize}ml | 3.4 oz.</span>
+                    <div class="item-price">$${item.productPrice} (introductory)</div>
+                </figcaption>
+            </div>
+        </div>
+    `
+})
+scrumProducts.forEach((item) => {
+    scrumcontent.innerHTML += `
+        <div class="col-md-3 ">
+            <div class="product-item">
+                <figure class="product-style">
+                    <img src="${item.productImage}" alt="${item.productName}" class="product-item">
+                    <button type="button" class="add-to-cart" data-product-tile="add-to-cart" onclick="addToCart(${item.id})">Add to Cart</button>
+                </figure>
+                <a href="${item.navigationLink}">
+                    <div class="text-center m-0 p-0">
+                        <button class="border rounded">Show details</button>
+                    </div>
+                </a>
+                <figcaption>
+                    <h3>${item.productName}</h3>
+                    <span>Size: ${item.productSize}ml | 3.4 oz.</span>
+                    <div class="item-price">$${item.productPrice} (introductory)</div>
+                </figcaption>
+            </div>
+        </div>
+    `
+})
+faceProducts.forEach((item) => {
+    facecontent.innerHTML += `
+        <div class="col-md-3 ">
+            <div class="product-item">
+                <figure class="product-style">
+                    <img src="${item.productImage}" alt="${item.productName}" class="product-item">
+                    <button type="button" class="add-to-cart" data-product-tile="add-to-cart" onclick="addToCart(${item.id})">Add to Cart</button>
+                </figure>
+                <a href="${item.navigationLink}">
+                    <div class="text-center m-0 p-0">
+                        <button class="border rounded">Show details</button>
+                    </div>
+                </a>
+                <figcaption>
+                    <h3>${item.productName}</h3>
+                    <span>Size: ${item.productSize}ml | 3.4 oz.</span>
+                    <div class="item-price">$${item.productPrice} (introductory)</div>
+                </figcaption>
+            </div>
+        </div>
+    `
+})
+hairProducts.forEach((item) => {
+    haircontent.innerHTML += `
+        <div class="col-md-3 ">
+            <div class="product-item">
+                <figure class="product-style">
+                    <img src="${item.productImage}" alt="${item.productName}" class="product-item">
+                    <button type="button" class="add-to-cart" data-product-tile="add-to-cart" onclick="addToCart(${item.id})">Add to Cart</button>
+                </figure>
+                <a href="${item.navigationLink}">
+                    <div class="text-center m-0 p-0">
+                        <button class="border rounded">Show details</button>
+                    </div>
+                </a>
+                <figcaption>
+                    <h3>${item.productName}</h3>
+                    <span>Size: ${item.productSize}ml | 3.4 oz.</span>
+                    <div class="item-price">$${item.productPrice} (introductory)</div>
+                </figcaption>
+            </div>
+        </div>
+    `
+})
+cleanserProducts.forEach((item) => {
+    cleansercontent.innerHTML += `
+        <div class="col-md-3 ">
+            <div class="product-item">
+                <figure class="product-style">
+                    <img src="${item.productImage}" alt="${item.productName}" class="product-item">
+                    <button type="button" class="add-to-cart" data-product-tile="add-to-cart" onclick="addToCart(${item.id})">Add to Cart</button>
+                </figure>
+                <a href="${item.navigationLink}">
+                    <div class="text-center m-0 p-0">
+                        <button class="border rounded">Show details</button>
+                    </div>
+                </a>
+                <figcaption>
+                    <h3>${item.productName}</h3>
+                    <span>Size: ${item.productSize}ml | 3.4 oz.</span>
+                    <div class="item-price">$${item.productPrice} (introductory)</div>
+                </figcaption>
+            </div>
+        </div>
     `
 })
 
